f7d1ddde0bf881a3e3678b1ef756d528b155d4d9
Guillermo Martinez <--global>
2017-03-17 21:09:23 +0100

    GI-Login acabada i interaccio amb altres activities

--- TwoChange/app/src/main/java/pes/twochange/LogIn.java
+++ TwoChange/app/src/main/java/pes/twochange/LogIn.java
@@ -1,7 +1,10 @@
 package pes.twochange;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LogIn extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class LogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        //Login button + Pressed button listener
+        Button loginBtn = (Button) findViewById(R.id.logInBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                Intent mainMenuIntent = new Intent (getApplicationContext(), zWorking.class);
+                startActivity(mainMenuIntent);
+            }
+        });
+
+        //Google button + Pressed button listener
+        Button googleBtn = (Button) findViewById(R.id.googleBtn);
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                Intent mainMenuIntent = new Intent (getApplicationContext(), zWorking.class);
+                startActivity(mainMenuIntent);
+            }
+        });
     }
 }
