dcb57b5ea247b04523555e994494eaca5d029703
visape <vikktor.1996@hotmail.com>
2017-03-24 22:53:51 +0100

    Loggin working

--- TwoChange/app/src/main/java/pes/twochange/LogIn.java
+++ TwoChange/app/src/main/java/pes/twochange/LogIn.java
@@ -2,19 +2,47 @@ package pes.twochange;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LogIn extends AppCompatActivity {
 
-    @Override
+    private static final String TAG = "LogIn";
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        //FIREBASE
+        mAuth = FirebaseAuth.getInstance();
+
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    //Logeado
+
+                } else {
+                    //No logeado
+                }
+            }
+        };
 
         //Login button + Pressed button listener
         Button loginBtn = (Button) findViewById(R.id.logInBtn);
@@ -22,25 +50,20 @@ public class LogIn extends AppCompatActivity {
             @Override public void onClick(View v) {
                 EditText user = (EditText)findViewById(R.id.mailField);
                 EditText pass = (EditText)findViewById(R.id.passwordField);
-                if (user.getText().toString().trim().equals("") || user.getText() == null) {
+
+                String email = user.getText().toString();
+                String password = pass.getText().toString();
+                if (email.isEmpty()) {
                     Context context = getApplicationContext();
-                    Toast toast = Toast.makeText(context, "Fill in the Email field", Toast.LENGTH_LONG);
-                    toast.show();
-                } else if (!user.getText().toString().contains("@")) {
+                    Toast.makeText(context, "Fill in the Email field", Toast.LENGTH_LONG).show();
+                } else if (!email.contains("@")) {
                     Context context = getApplicationContext();
-                    Toast toast = Toast.makeText(context, "Incorrect Email format", Toast.LENGTH_LONG);
-                    toast.show();
-                } else if (pass.getText().toString().trim().equals("") || pass.getText() == null) {
+                    Toast.makeText(context, "Incorrect Email format", Toast.LENGTH_LONG).show();
+                } else if (password.isEmpty()) {
                     Context context = getApplicationContext();
-                    Toast toast = Toast.makeText(context, "Fill in the password field", Toast.LENGTH_LONG);
-                    toast.show();
-                } else if (/*logIn(user, pass)*/ true) {
-                    Intent mainMenuIntent = new Intent (getApplicationContext(), zWorking.class);
-                    startActivity(mainMenuIntent);
+                    Toast.makeText(context, "Fill in the password field", Toast.LENGTH_LONG).show();
                 } else {
-                    Context context = getApplicationContext();
-                    Toast toast = Toast.makeText(context, "Wrong user or password", Toast.LENGTH_LONG);
-                    toast.show();
+                    logIn(email, password);
                 }
             }
         });
@@ -62,5 +85,37 @@ public class LogIn extends AppCompatActivity {
                 startActivity(mainMenuIntent);
             }
         });
+    }
+
+    private void logIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(LogIn.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithEmail:success");
+                            Intent mainMenuIntent = new Intent (getApplicationContext(), zWorking.class);
+                            startActivity(mainMenuIntent);
+                        } else {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Context context = getApplicationContext();
+                            Toast.makeText(context, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
