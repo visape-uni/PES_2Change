97e737a596a5083bf765969b4d9aeedf39d60654
Guillermo Martinez <--global>
2017-03-29 18:55:39 +0200

    Interficie de newUser acabada

--- TwoChange/app/src/main/java/pes/twochange/NewUser.java
+++ TwoChange/app/src/main/java/pes/twochange/NewUser.java
@@ -2,8 +2,11 @@ package pes.twochange;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,12 +20,12 @@ public class NewUser extends AppCompatActivity {
         setContentView(R.layout.activity_new_user);
 
         //Sign Up button + Pressed button listener
-        Button googleBtn = (Button)findViewById(R.id.signUpBtn);
-        googleBtn.setOnClickListener(new View.OnClickListener() {
+        Button signUpBtn = (Button)findViewById(R.id.signUpBtn);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override public void onClick(View v) {
                 EditText nameText = (EditText)findViewById(R.id.nameField);
                 EditText mailText = (EditText)findViewById(R.id.mailField);
-                EditText passText = (EditText)findViewById(R.id.passwordField);
+                EditText passText = (EditText)findViewById(R.id.passField);
                 EditText repePassText = (EditText)findViewById(R.id.repeatPassField);
                 String name = nameText.getText().toString().trim();
                 String mail = mailText.getText().toString().trim();
@@ -46,10 +49,27 @@ public class NewUser extends AppCompatActivity {
                     Context context = getApplicationContext();
                     Toast.makeText(context, "Fill in the repeated password field", Toast.LENGTH_LONG).show();
                 } else {
+                    //ADRIIII BORRA LAS DOS LINEAS DE DEBAJO Y MANDA AQUI EL REGISTER
                     Intent mainMenuIntent = new Intent (getApplicationContext(), zWorking.class);
                     startActivity(mainMenuIntent);
                 }
             }
         });
+
+        //Matches both passwords in real time and changes foreground color
+        final EditText repePassText = (EditText)findViewById(R.id.repeatPassField);
+        repePassText.addTextChangedListener((new TextWatcher(){
+            EditText passText = (EditText)findViewById(R.id.passField);
+            public void afterTextChanged(Editable s) {
+                if (!(repePassText.getText().toString().equals(passText.getText().toString()))) {
+                    repePassText.setTextColor(Color.RED);
+                } else {
+                    repePassText.setTextColor(Color.BLACK);
+                }
+
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count){}
+        }));
     }
 }
