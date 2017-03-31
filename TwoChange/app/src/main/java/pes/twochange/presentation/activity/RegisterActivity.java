492d561bc85d4bd6efd89fa90e65db64b08e1907
visape-uni <visape.uni@gmail.com>
2017-03-31 17:50:32 +0200

    register update

--- TwoChange/app/src/main/java/pes/twochange/presentation/activity/RegisterActivity.java
+++ TwoChange/app/src/main/java/pes/twochange/presentation/activity/RegisterActivity.java
@@ -3,23 +3,45 @@ package pes.twochange.presentation.activity;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import pes.twochange.R;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "RegisterActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+            }
+        };
+
 
         //Sign Up button + Pressed button listener
         Button signUpBtn = (Button)findViewById(R.id.signUpBtn);
@@ -51,9 +73,26 @@ public class RegisterActivity extends AppCompatActivity {
                     Context context = getApplicationContext();
                     Toast.makeText(context, "Fill in the repeated password field", Toast.LENGTH_LONG).show();
                 } else {
-                    //ADRIIII BORRA LAS DOS LINEAS DE DEBAJO Y MANDA AQUI EL REGISTER
-                    Intent mainMenuIntent = new Intent (getApplicationContext(), zWorking.class);
-                    startActivity(mainMenuIntent);
+
+                    mAuth.createUserWithEmailAndPassword(mail, pass)
+                            .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(RegisterActivity.this,"User created",
+                                                Toast.LENGTH_LONG).show();
+                                        Intent mainMenuIntent = new Intent(getApplicationContext(), zWorking.class);
+                                        startActivity(mainMenuIntent);
+                                        finish();
+                                    }
+                                    else if (!task.isComplete()){
+                                        Toast.makeText(RegisterActivity.this,"Error",
+                                                Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            });
+
                 }
             }
         });
