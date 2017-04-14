850635399da7db1adac4c5bff974af7bf9ec68ec
AndresInsaurralde <andres.insaurralde@est.fib.upc.edu>
2017-04-14 18:39:41 +0200

    Layout

--- TwoChange/app/src/main/java/pes/twochange/presentation/activity/PostAdActivity.java
+++ TwoChange/app/src/main/java/pes/twochange/presentation/activity/PostAdActivity.java
@@ -5,11 +5,15 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import pes.twochange.R;
 import pes.twochange.domain.model.Ad;
 
 public class PostAdActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = "AppCompatActivity";
+    private static final int IMAGE_PICK_CODE = 0;
 
     private Ad ad;
 
@@ -19,19 +23,14 @@ public class PostAdActivity extends AppCompatActivity {
         setContentView(R.layout.activity_post_ad);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
-        Intent pickPhoto = new Intent(Intent.ACTION_PICK,
-                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(pickPhoto , 0);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
         super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
         switch(requestCode) {
-            case 0:
-                if(resultCode == RESULT_OK){
+            case IMAGE_PICK_CODE:
+                if (resultCode == RESULT_OK){
                     Uri selectedImage = imageReturnedIntent.getData();
 
                 }
@@ -40,7 +39,18 @@ public class PostAdActivity extends AppCompatActivity {
         }
     }
 
-    private void publish() {
+    /*
+        Asks the user to select an image from either camera or gallery
+        and adds it to the ad. It also modifies the ImageButton to show a
+        thumbnail of the image.
+     */
+    public void selectImage(View v) {
+        Intent pickPhoto = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(pickPhoto , 0);
+    }
+
+    public void publish(View v) {
 
     }
 }
