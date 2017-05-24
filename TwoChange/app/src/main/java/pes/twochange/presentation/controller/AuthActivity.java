24d8a94ea0200d0a995ca223c54b0c5c66dd260f
Fèlix Arribas Pardo <felixarpa@gmail.com>
2017-05-24 17:48:45 +0200

    AuthActivity creation

---
+++ TwoChange/app/src/main/java/pes/twochange/presentation/controller/AuthActivity.java
@@ -0,0 +1,15 @@
+package pes.twochange.presentation.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import pes.twochange.R;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
