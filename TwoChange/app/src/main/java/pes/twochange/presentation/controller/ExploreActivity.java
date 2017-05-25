fce0404a5971ee4d6f4637a2fd2714278101e1da
Fèlix Arribas Pardo <felixarpa@gmail.com>
2017-05-25 10:50:09 +0200

    Search Input Layout

--- TwoChange/app/src/main/java/pes/twochange/presentation/controller/ExploreActivity.java
+++ TwoChange/app/src/main/java/pes/twochange/presentation/controller/ExploreActivity.java
@@ -2,6 +2,7 @@ package pes.twochange.presentation.controller;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 
 import java.util.ArrayList;
 
@@ -22,7 +23,7 @@ public class ExploreActivity extends BaseActivity implements ProductsListFragmen
 
         fragment = ProductsListFragment.newInstance();
 
-        addFragment(R.id.explore_frame, fragment, "Products List");
+        replaceFragment(R.id.explore_frame, fragment);
 
     }
 
@@ -31,21 +32,46 @@ public class ExploreActivity extends BaseActivity implements ProductsListFragmen
         return EXPLORE_ACTIVITY;
     }
 
-    private ArrayList<Ad> productsList;
+    private ArrayList<Ad> productsList = new ArrayList<>();
 
     @Override
     public void loadProductList() {
-        // TODO:
+        // TODO
         // Descarga todos los productos de Firebase sin importar ninguna libreria ni clase de
         // firebase aqui. Esto es la capa de presentación, firebase es la de datos. Hay que pasar
         // por un Theme/Controlador/etc.
+        //
+        // TODO
+        // Download all the products from Firebase without importing any Firebase library neither
+        // class here. This is the presentation layer, Firebase is data layer. You have to give the
+        // data through a Theme/Controller/etc.
         if (fragment != null && fragment instanceof ProductsListFragment) {
             ((ProductsListFragment) fragment).display(productsList);
+        }
+    }
+
+
+    private ArrayList<Ad> searchResultList = new ArrayList<>();
+
+    @Override
+    public void searchProducts(String query) {
+        // TODO
+        // Descarga productos que contenga $query de Firebase
+        //
+        // TODO
+        // Download product that contain $query from Firebase
+        if (fragment != null && fragment instanceof ProductsListFragment) {
+            ((ProductsListFragment) fragment).display(searchResultList);
         }
     }
 
     @Override
     public void onRecyclerViewItemClickListener(int position) {
 
+    }
+
+    @Override
+    public Toolbar getToolbar() {
+        return toolbar;
     }
 }
--- TwoChange/app/src/main/java/pes/twochange/presentation/fragment/ProductsListFragment.java
+++ TwoChange/app/src/main/java/pes/twochange/presentation/fragment/ProductsListFragment.java
@@ -6,9 +6,13 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 
@@ -17,10 +21,11 @@ import pes.twochange.domain.model.Ad;
 import pes.twochange.presentation.adapter.RecyclerViewProductAdapter;
 import pes.twochange.presentation.view.OnRecyclerViewItemClickListener;
 
-public class ProductsListFragment extends Fragment {
+public class ProductsListFragment extends Fragment implements TextWatcher {
 
     private OnFragmentInteractionListener activity;
     private RecyclerView recyclerView;
+    private EditText editText;
 
     public ProductsListFragment() {
         // Required empty public constructor
@@ -41,6 +46,8 @@ public class ProductsListFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_products_list, container, false);
 
         recyclerView = (RecyclerView) view.findViewById(R.id.products_recycler_view);
+        editText = (EditText) view.findViewById(R.id.search_edit_text);
+        editText.addTextChangedListener(this);
 
         return view;
     }
@@ -73,11 +80,28 @@ public class ProductsListFragment extends Fragment {
         recyclerView.setAdapter(adapter);
     }
 
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        String query = s.toString();
+        if (query.length() == 0) {
+            activity.loadProductList();
+        } else {
+            activity.searchProducts(query);
+        }
+    }
 
 
     public interface OnFragmentInteractionListener  extends OnRecyclerViewItemClickListener {
 
         void loadProductList();
+        void searchProducts(String query);
+        Toolbar getToolbar();
 
     }
 
