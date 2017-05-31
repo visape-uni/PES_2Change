970aef5d43f14777a91d4410da77c4c1fe46bff3
Guillermo <guillermomm1230@gmail.com>
2017-05-31 13:50:10 +0200

    Interficie register acabada, falta crida a metodes

--- TwoChange/app/src/main/java/pes/twochange/presentation/activity/NewUserActivity.java
+++ TwoChange/app/src/main/java/pes/twochange/presentation/activity/NewUserActivity.java
@@ -31,7 +31,7 @@ public class NewUserActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_new_user);
+        //setContentView(R.layout.activity_new_user);
 
 
         //Next Step button + Pressed button listener
