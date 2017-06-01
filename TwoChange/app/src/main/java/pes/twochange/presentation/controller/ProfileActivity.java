212ff5ebd825ff2b89c42bf0cfb252900b3c3b9f
visape <vikktor.1996@hotmail.com>
2017-06-01 17:17:29 +0200

    Layout profile activity

---
+++ TwoChange/app/src/main/java/pes/twochange/presentation/controller/ProfileActivity.java
@@ -0,0 +1,20 @@
+package pes.twochange.presentation.controller;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import pes.twochange.R;
+
+public class ProfileActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+}
