107733bb14ebd7d3f5c6d2725669b0f5427a86b8
Guillermo <guillermomm1230@gmail.com>
2017-06-06 22:50:28 +0200

    Vistes de login i derivats acabades

--- TwoChange/app/src/main/java/pes/twochange/presentation/controller/AuthActivity.java
+++ TwoChange/app/src/main/java/pes/twochange/presentation/controller/AuthActivity.java
@@ -1,29 +1,52 @@
 package pes.twochange.presentation.controller;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.EditText;
 import android.widget.Toast;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import pes.twochange.R;
+import pes.twochange.domain.callback.ProfileResponse;
+import pes.twochange.domain.model.Profile;
 import pes.twochange.domain.themes.AuthTheme;
+import pes.twochange.domain.themes.ProfileTheme;
 import pes.twochange.presentation.Config;
-import pes.twochange.presentation.activity.EditProfileActivity;
 import pes.twochange.presentation.fragment.LoginFragment;
+import pes.twochange.presentation.fragment.NewProfileFragment;
 import pes.twochange.presentation.fragment.RegisterFragment;
+import pes.twochange.services.Firebase;
 
 public class AuthActivity extends AppCompatActivity implements
         LoginFragment.OnFragmentInteractionListener, RegisterFragment.OnFragmentInteractionListener,
+        NewProfileFragment.OnFragmentInteractionListener,
         AuthTheme.Response, GoogleApiClient.OnConnectionFailedListener {
     //Attributes
     private static final int RC_SIGN_IN = 9001;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "New User Fragment";
     private GoogleApiClient googleApiClient;
     private FragmentManager fragmentManager;
     private Fragment fragment;
@@ -41,11 +64,16 @@ public class AuthActivity extends AppCompatActivity implements
                     .replace(R.id.container, fragment)
                     .commit();
         } else if (getIntent().getExtras().getString("startPoint").equals("PROFILE")) {
-            /*fragment = ProfileFragment.newInstance();
+            fragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+            fragment = LoginFragment.newInstance();
             fragmentManager.beginTransaction()
                     .replace(R.id.container, fragment)
-                    .commit();*/
-            Toast.makeText(getApplicationContext(), "Profiles Fragment", Toast.LENGTH_LONG).show();
+                    .commit();
+            fragment = NewProfileFragment.newInstance();
+            fragmentManager.beginTransaction()
+                    .add(R.id.container, fragment)
+                    .addToBackStack("Set Profile")
+                    .commit();
         }
         //Configurar Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -92,13 +120,21 @@ public class AuthActivity extends AppCompatActivity implements
     }
     @Override
     public void profile() {
-        /*fragment = SignupFragment.newInstance();
-        addFragment("Sign Up");*/
-        Toast.makeText(getApplicationContext(), "Hej pussies I'm here!", Toast.LENGTH_LONG).show();
+        fragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+        fragment = LoginFragment.newInstance();
+        fragmentManager.beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
+        fragment = NewProfileFragment.newInstance();
+        fragmentManager.beginTransaction()
+                .add(R.id.container, fragment)
+                .addToBackStack("Set Profile")
+                .commit();
     }
     @Override
     public void noConnection() {
         // TODO Control d'errors
+        Toast.makeText(AuthActivity.this, "Unknown Error: Check your credentials", Toast.LENGTH_LONG).show();
     }
 
     //Register button pressed listener
@@ -128,5 +164,92 @@ public class AuthActivity extends AppCompatActivity implements
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
+    }
+
+    @Override
+    public void onCreateUser(final String newMail, final String newPass) {
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.createUserWithEmailAndPassword(newMail, newPass).addOnCompleteListener(AuthActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "User successfully signed up", Toast.LENGTH_LONG).show();
+                    fragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                    fragment = LoginFragment.newInstance();
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.container, fragment)
+                            .commit();
+                    fragment = NewProfileFragment.newInstance();
+                    fragmentManager.beginTransaction()
+                            .add(R.id.container, fragment)
+                            .addToBackStack("Set Profile")
+                            .commit();
+                } else {
+                    try {
+                        throw task.getException();
+                    } catch(FirebaseAuthWeakPasswordException e) {
+                        //TODO Fix: Quan hauria de saltar aquesta excepcio, salta la Unknown
+                        EditText passText = (EditText)findViewById(R.id.passField);
+                        EditText repePassText = (EditText)findViewById(R.id.repeatPassField);
+                        Toast.makeText(AuthActivity.this, "Error: Password must be larger", Toast.LENGTH_LONG).show();
+                        passText.setText("");
+                        repePassText.setText("");
+                        passText.requestFocus();
+                    } catch(FirebaseAuthInvalidCredentialsException e) {
+                        EditText mailText = (EditText)findViewById(R.id.mailField);
+                        Toast.makeText(AuthActivity.this, "Error: Invalid Email Format", Toast.LENGTH_LONG).show();
+                        mailText.setText("");
+                        mailText.requestFocus();
+                    } catch(FirebaseAuthUserCollisionException e) {
+                        EditText mailText = (EditText)findViewById(R.id.mailField);
+                        Toast.makeText(AuthActivity.this, "Error: This Email is Already Registered", Toast.LENGTH_LONG).show();
+                        mailText.setText("");
+                        mailText.requestFocus();
+                    } catch(Exception e) {
+                        Toast.makeText(AuthActivity.this, "Unknown Error: Check your credentials", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onSetProfileClick(final Profile profile) {
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        ref.child("profile").child(profile.getUsername()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    Toast.makeText(getApplication(), "There is already a user with this username"
+                            , Toast.LENGTH_LONG).show();
+                } else {
+                    mAuth = FirebaseAuth.getInstance();
+                    final String uid = mAuth.getCurrentUser().getUid();
+                    profile.setUid(uid);
+                    ProfileTheme.getInstance(profile).updateProfile(
+                            new ProfileResponse() {
+                                @Override
+                                public void success(Profile profile) {
+                                    SharedPreferences.Editor editor = getSharedPreferences(Config.SP_NAME, MODE_PRIVATE).edit();
+                                    editor.putString("username", profile.getUsername());
+                                    editor.putString("uid", uid);
+                                    editor.apply();
+                                    startActivity(new Intent(getApplicationContext(), ExploreActivity.class));
+                                    finish();
+                                }
+                                @Override
+                                public void failure(String s) {
+                                    Toast.makeText(getApplication(), "Internal Error", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                    );
+
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
     }
 }
--- TwoChange/app/src/main/java/pes/twochange/presentation/fragment/RegisterFragment.java
+++ TwoChange/app/src/main/java/pes/twochange/presentation/fragment/RegisterFragment.java
@@ -1,12 +1,17 @@
 package pes.twochange.presentation.fragment;
 
 import android.content.Context;
-import android.net.Uri;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import pes.twochange.R;
 
@@ -22,6 +27,50 @@ public class RegisterFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_register, container, false);
+
+        final EditText mailText = (EditText)view.findViewById(R.id.mailField);
+        final EditText passText = (EditText)view.findViewById(R.id.passField);
+        final EditText repePassText = (EditText)view.findViewById(R.id.repeatPassField);
+
+        //Next Step button + Pressed button listener
+        Button nextBtn = (Button)view.findViewById(R.id.nextBtn);
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                Context context = getActivity();
+                final String mail = mailText.getText().toString().trim();
+                final String pass = passText.getText().toString().trim();
+                String repePass = repePassText.getText().toString().trim();
+                if (mail.isEmpty() || mailText.getText() == null) {
+                    Toast.makeText(context, "Fill in the Email field", Toast.LENGTH_LONG).show();
+                    mailText.requestFocus();
+                } else if (pass.isEmpty() || passText.getText() == null) {
+                    Toast.makeText(context, "Fill in the password field", Toast.LENGTH_LONG).show();
+                    passText.requestFocus();
+                } else if (repePass.isEmpty() || repePassText.getText() == null) {
+                    Toast.makeText(context, "Fill in the repeated password field", Toast.LENGTH_LONG).show();
+                    repePassText.requestFocus();
+                } else if (!(repePassText.getText().toString().equals(passText.getText().toString()))) {
+                    Toast.makeText(context, "Both passwords must be equal", Toast.LENGTH_LONG).show();
+                    repePassText.setText("");
+                    repePassText.requestFocus();
+                } else {
+                    activity.onCreateUser(mail, pass);
+                }
+            }
+        });
+
+        //Matches both passwords in real time and changes foreground color
+        repePassText.addTextChangedListener((new TextWatcher(){
+            public void afterTextChanged(Editable s) {
+                if (!(repePassText.getText().toString().equals(passText.getText().toString()))) {
+                    repePassText.setTextColor(Color.RED);
+                } else {
+                    repePassText.setTextColor(Color.BLACK);
+                }
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count){}
+        }));
 
         return view;
     }
@@ -45,8 +94,6 @@ public class RegisterFragment extends Fragment {
 
     //Methods implemented by the host activity
     public interface OnFragmentInteractionListener {
-        void onLoginClick(String email, String password);
-        void onRegisterClick();
-        void onLoginWithGoogle();
+        void onCreateUser(String mail, String password);
     }
 }
