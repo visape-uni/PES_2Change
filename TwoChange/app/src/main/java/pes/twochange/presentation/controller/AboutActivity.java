1d6fd1ec335d999159d479c65e60c2f8d5d8e3de
visape-uni <visape.uni@gmail.com>
2017-06-08 19:01:01 +0200

    Millorar vista del about

--- TwoChange/app/src/main/java/pes/twochange/presentation/controller/AboutActivity.java
+++ TwoChange/app/src/main/java/pes/twochange/presentation/controller/AboutActivity.java
@@ -13,12 +13,5 @@ public class AboutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        TextView field = (TextView)findViewById(R.id.aboutField);
-        field.setText("Project 2Change\n\nDeveloped by:" +
-                "\nVictor Sánchez" +
-                "\nFèlix Arribas" +
-                "\nGuillermo Martínez" +
-                "\nAdrián Muñoz" +
-                "\nAndrés Insaurralde");
     }
 }
